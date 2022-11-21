5aea5ea29ae528b7e3a8b009563cdc6e0a1c56ef
juliana <julianacmenezes@msn.com>
2022-11-21 20:54:35 -0300

    add itemlist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import NavBar from "./componentes/NavBar";
 import CartWidget from "./componentes/CartWidget";
+import ItemListContainer from "./componentes/ItemListContainer"; 
 
 const App = ()=> {
   return (
-    <NavBar></NavBar>
+
+    <div>
+
+    
+      <NavBar></NavBar>
+
+      {/* Mensaje de bienvenida en ItemListContainer.jsx */}
+      <ItemListContainer 
+      texto = 'Bienvenido a la tienda de mangas y figuras de Juliana!'
+      ></ItemListContainer>
+      
+
+    
+    </div>
+    
+    
   );
 }
 
---
+++ src/componentes/ItemListContainer.jsx
@@ -0,0 +1,22 @@
+//Mensagem de bienvenida//
+const ItemListContainer = ({texto})=>{
+    return(
+        
+        <h1 style={{color: 'pink', fontSize:'2rem', textAlign:'center', padding: 15}}>{texto}</h1>
+
+    )
+}
+
+export default ItemListContainer;
+
+
+//Primera tentativa
+// const ItemListContainer = ()=>{
+//     return(
+
+//         <h1 style={{color: 'pink', fontSize:'2rem', textAlign:'center', padding: 15 }}>Bienvenido a la tienda de mangas y figuras de Juliana!</h1>
+
+//     )
+// }
+
+// export default ItemListContainer;
